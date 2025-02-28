82f9f86467fbe0dd2e651d8fff307f7ffe8a7e6f
Ilchenko55Artem <159420916+Ilchenko55Artem@users.noreply.github.com>
2025-02-28 13:27:05 +0200

    Add files via upload

---
+++ лаб 4 1 завдання/лаб 4 1 завдання/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть кількість елементів масиву: ");
+        int n = int.Parse(Console.ReadLine());
+        double[] array = new double[n];
+        Console.WriteLine("Введіть елементи масиву:");
+        for (int i = 0; i < n; i++) array[i] = double.Parse(Console.ReadLine());
+        int minAbsIndex = FindMinAbsIndex(array);
+        Console.WriteLine($"Номер мінімального за модулем елемента: {minAbsIndex}");
+        double sumAfterFirstNegative = SumAbsAfterFirstNegative(array);
+        Console.WriteLine($"Сума модулів елементів після першого від’ємного: {sumAfterFirstNegative}");
+        Console.Write("Введіть межі інтервалу для видалення (min max): ");
+        string[] bounds = Console.ReadLine().Split();
+        double minBound = double.Parse(bounds[0]), maxBound = double.Parse(bounds[1]);
+        array = CompressArray(array, minBound, maxBound);
+        Console.WriteLine("Оновлений масив:");
+        Console.WriteLine(string.Join(" ", array));
+    }
+    static int FindMinAbsIndex(double[] array)
+    {
+        int index = 0;
+        double minAbsValue = Math.Abs(array[0]);
+        for (int i = 1; i < array.Length; i++)
+            if (Math.Abs(array[i]) < minAbsValue) { minAbsValue = Math.Abs(array[i]); index = i; }
+        return index;
+    }
+    static double SumAbsAfterFirstNegative(double[] array)
+    {
+        int firstNegIndex = Array.FindIndex(array, x => x < 0);
+        if (firstNegIndex == -1 || firstNegIndex == array.Length - 1) return 0;
+        return array.Skip(firstNegIndex + 1).Select(Math.Abs).Sum();
+    }
+    static double[] CompressArray(double[] array, double minBound, double maxBound)
+    {
+        double[] compressed = array.Where(x => x < minBound || x > maxBound).ToArray();
+        return compressed.Concat(new double[array.Length - compressed.Length]).ToArray();
+    }
+}
---
+++ лаб 4 2 завдання/лаб 4 2 завдання/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[,] array = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12}
+        };
+        Console.WriteLine("Масив:");
+        PrintArray(array);
+        Console.Write("Введіть два індекси (i1, i2) для суми елементів 3-го стовпця: ");
+        int i1 = int.Parse(Console.ReadLine()), i2 = int.Parse(Console.ReadLine());
+        if (i1 < array.GetLength(0) && i2 < array.GetLength(0))
+            Console.WriteLine($"Сума: {array[i1, 2] + array[i2, 2]}");
+        else Console.WriteLine("Невірні індекси!");
+
+        Console.Write("Введіть два індекси (j1, j2) для добутку елементів 2-го рядка: ");
+        int j1 = int.Parse(Console.ReadLine()), j2 = int.Parse(Console.ReadLine());
+        if (j1 < array.GetLength(1) && j2 < array.GetLength(1))
+            Console.WriteLine($"Добуток: {array[1, j1] * array[1, j2]}");
+        else Console.WriteLine("Невірні індекси!");
+    }
+    static void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+                Console.Write(array[i, j] + "\t");
+            Console.WriteLine();
+        }
+    }
+}
