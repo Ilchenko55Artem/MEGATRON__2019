0dfc4cb1b5c19a2b9231f1674f24d144d4ad863f
Ilchenko55Artem <159420916+Ilchenko55Artem@users.noreply.github.com>
2025-02-28 13:34:59 +0200

    Add files via upload

---
+++ лаб 5 1 завдання/лаб 5 1 завдання/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть від 1 до 20 слів через пробіл:");
+        string input = Console.ReadLine();
+        string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        Console.WriteLine("Послідовність слів без повторень:");
+        Console.WriteLine(string.Join(" ", words.Distinct()));
+
+        Console.WriteLine("Слова, що зустрічаються тільки один раз:");
+        var uniqueWords = words.GroupBy(w => w).Where(g => g.Count() == 1).Select(g => g.Key);
+        Console.WriteLine(string.Join(" ", uniqueWords));
+    }
+}
